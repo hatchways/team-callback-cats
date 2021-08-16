13be18f850120eb7d1b25517157522755b67fb47
Damien Aries <damien.aries@gmail.com>
2021-08-16 12:52:11 -0700

    Public links minor MUI changes before merge

--- client/src/components/Navigation/PublicLinks.tsx
+++ client/src/components/Navigation/PublicLinks.tsx
@@ -2,15 +2,16 @@ import { FC } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import useStyles from './useStyles';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
 
 const PublicLinks: FC = () => {
   const classes = useStyles();
   const history = useHistory();
 
   return (
-    <ul className={classes.publicLinks}>
+    <List component="ul" className={classes.publicLinks}>
       <Link to="/become-a-sitter" className={classes.sitterLink}>
-        <a>Become a sitter</a>
+        Become a sitter
       </Link>
       <Button variant="outlined" size="large" className={classes.login} onClick={() => history.push('/login')}>
         Login
@@ -18,7 +19,7 @@ const PublicLinks: FC = () => {
       <Button variant="contained" size="large" className={classes.signUp} onClick={() => history.push('/signup')}>
         Sign Up
       </Button>
-    </ul>
+    </List>
   );
 };
 
