4da6f80e92eaff3c0c05c1271f7bb09c4215048c
Damien Aries <damien.aries@gmail.com>
2021-08-24 20:51:39 -0700

    Listings page skeleton

--- client/src/pages/Listings/Listings.tsx
+++ client/src/pages/Listings/Listings.tsx
@@ -4,9 +4,9 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import { DatePicker } from '@material-ui/pickers';
-import ListingCard from './ListingCard/ListingCard';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+// import ListingCard from './ListingCard/ListingCard';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Listings: FC = () => {
   const classes = useStyles();
@@ -16,11 +16,16 @@ const Listings: FC = () => {
     <Box className={classes.root}>
       <Typography variant="h3">Your search results</Typography>
       <Box className={classes.searchContainer}>
-        <TextField>
-          <FontAwesomeIcon icon={faSearch} />
-        </TextField>
-        <DatePicker value={selectedDate} label="Select Date" onChange={handleDateChange} clearable />
+        <TextField className={classes.searchInput}>{/* <FontAwesomeIcon icon={faSearch} /> */}</TextField>
+        <DatePicker
+          value={selectedDate}
+          label="Select Date"
+          onChange={handleDateChange}
+          className={classes.searchInput}
+          clearable
+        />
       </Box>
+      <Box className={classes.listingsContainer}></Box>
     </Box>
   );
 };
--- client/src/pages/Listings/useStyles.ts
+++ client/src/pages/Listings/useStyles.ts
@@ -6,6 +6,15 @@ const useStyles = makeStyles(() => ({
   },
   searchContainer: {
     border: '1px solid red',
+    display: 'flex',
+    margin: '1rem 0',
+  },
+  searchInput: {
+    border: '1px solid rgba(1, 1, 1, .5)',
+  },
+  listingsContainer: {
+    margin: '2rem 0',
+    border: '1px solid red',
   },
 }));
 
