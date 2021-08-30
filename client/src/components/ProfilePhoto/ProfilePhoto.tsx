f54d3d8188ebb3332a220604975aadbbf284a4ce
Damien Aries <damien.aries@gmail.com>
2021-08-30 12:41:00 -0700

    initial profile photo upload changes

--- client/src/components/ProfilePhoto/ProfilePhoto.tsx
+++ client/src/components/ProfilePhoto/ProfilePhoto.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC, useState } from 'react';
 import useStyles from './useStyles';
 import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
@@ -10,11 +10,18 @@ import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 const ProfilePhoto: FC = () => {
   const classes = useStyles();
+  const [selectedFile, setSelectedFile] = useState({});
 
   // Upload Image
-  // TODO hook up to upload route: images in s3, save url in mongoDB
-  const handleUpload = () => {
-    console.log('You are trying to upload an image');
+  // TODO hook up to upload route: images in s3, save url in User profile
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    setSelectedFile(e.target.files[0]);
+    console.log(selectedFile);
+    // Pass file to upload function
+    // upload user profile with image url
   };
 
   // Delete Image
@@ -35,16 +42,34 @@ const ProfilePhoto: FC = () => {
         <Typography variant="h3">Profile Photo</Typography>
         <Avatar src={JoePlaceholder} alt="User Profile Photo" className={classes.avatar} />
         <Typography variant="subtitle1">Be sure to use a photo that clearly shows your face</Typography>
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="upload-button-file"
+          multiple
+          type="file"
+          onChange={handleFile}
+        />
+        <label htmlFor="upload-button-file">
+          <Button
+            variant="outlined"
+            color="secondary"
+            component="span"
+            fullWidth={true}
+            startIcon={<CloudUploadIcon />}
+            className={classes.cta}
+          >
+            Upload a file from your device
+          </Button>
+        </label>
+
         <Button
-          variant="outlined"
-          startIcon={<CloudUploadIcon />}
-          className={classes.cta}
-          onClick={handleUpload}
-          color="secondary"
+          variant="text"
+          component="button"
+          fullWidth={true}
+          onClick={handleDelete}
+          startIcon={<DeleteOutlineIcon />}
         >
-          Upload a file from your device
-        </Button>
-        <Button className={classes.cta} onClick={handleDelete} startIcon={<DeleteOutlineIcon />}>
           Delete Photo
         </Button>
       </Box>
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -7,6 +7,16 @@ const config = require('../utils/s3Config');
 
 const s3 = new aws.S3(config);
 
+// filter for image formats only
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+// config ulpoad function, apply filter
 const upload = multer({
     storage: multerS3({
         s3: s3,
@@ -17,13 +27,38 @@ const upload = multer({
         key: function(req, file, cb) {
             cb(null, Date.now().toString());
         }
-    })
+    }),
+    fileFilter: fileFilter,
 })
 
-// upload route | Allow 5 images
+// upload route for multiple images
 router.post('/upload-image', upload.array('photos', 5), (req, res, next) => {
     res.send(`Successfully uploaded ${req.files.length} files!`);
 });
 
+// upload single (profile picture)
+// const profileImageUpload = upload.single('image');
+// router.post('/:id/update-profile-picture', (req, res) => {
+//     const userId = req.params.id;
+//     profileImageUpload(req, res, function(err) {
+//         if (err) {
+//             return res.json({
+//                 success: false,
+//                 errors: {
+//                     title: "Image Upload Error",
+//                     detail: err.message,
+//                     error: err
+//                 }
+//             })
+//         }
+//         let update = { profilePic: req.file.location };
+
+//         User.findByIdAndUpdate(userId, update)
+//         .then((user) => res.status(200).json({ success: true, user: user }))
+//         .catch((err) => res.status(400).json({ success: false, error: err }))
+//     })
+// })
 
 module.exports = router;
+
+module.exports = upload;
