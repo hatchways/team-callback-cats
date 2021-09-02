a2d557656d4933d3d2a7a9b1308416a2e042ed3a
Damien Aries <damien.aries@gmail.com>
2021-09-02 15:44:50 -0700

    initial commit listing detail

--- client/src/pages/Listings/ListingDetail/ListingDetail.tsx
+++ client/src/pages/Listings/ListingDetail/ListingDetail.tsx
@@ -1,40 +1,30 @@
-import { useState, useEffect } from 'react';
+// import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import useStyles from './useStyles';
-
-const dummyListing = {
-  headerImg: 'https://source.unsplash.com/random',
-  profilePic: 'https://source.unsplash.com/random',
-  id: 1,
-  username: 'Norma Byers',
-  subtitle: ' Loving Dog Sitter',
-  city: 'Toronto, Canada',
-  rate: 14,
-  details:
-    'Kombucha tumblr kickstarter salvia XOXO heirloom cold-pressed schlitz, vegan etsy plaid next level. Yuccie listicle jean shorts ramps chambray, quinoa distillery PBR&B iceland. Enamel pin flannel chia austin keffiyeh scenester quinoa single-origin coffee crucifix. Squid single-origin coffee affogato ramps messenger bag chillwave, vape salvia echo park fanny pack semiotics flannel pinterest.',
-  rating: 4,
-  additionalPics: [
-    'https://source.unsplash.com/random',
-    'https://source.unsplash.com/random',
-    'https://source.unsplash.com/random',
-  ],
-};
+import { username, rate } from '../dummyListing';
+import { Listing } from '../../../interface/Listing';
+interface LocationType {
+  state: Listing;
+}
 
 const ListingDetail = (): JSX.Element => {
   const classes = useStyles();
-  const [listing, setListing] = useState();
+  const location: LocationType = useLocation();
+  const listing = location.state;
 
-  const { id, profilePic, username, subtitle, rating, details, city, rate } = dummyListing;
+  console.log(listing);
+
+  //   const { id, profilePic, username, subtitle, rating, details, city, rate } = listing;
 
   return (
     <Box display="flex" justifyContent="center" alignItems="flex-start" p={3} m={3} className={classes.root}>
       <Box m={3} p={2} className={classes.details}>
-        <p>details</p>
+        <p>Details</p>
       </Box>
       <Box m={3} p={2} className={classes.bookingContainer}>
-        <p>Booking</p>
+        <p>booking</p>
       </Box>
     </Box>
   );
